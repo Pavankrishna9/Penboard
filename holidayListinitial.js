3e119bb9252348cbeef83be44f9ac83d9a880277
nallagsoc <vivek.candid@gmail.com>
2014-10-08 19:58:58 +0530

    updated holidayListInitial

---
+++ holidayListinitial.js
@@ -0,0 +1,16 @@
+/* Copyright (c) 2014 Vivek Nalla <vivek.candid@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or  * without modification, are permitted provided that the 
+ * redistributions of source code must retain the above
+ * copyright notice
+ */
+
+select_namespace("dff.settings.d2t.holidaylist", function(namespace) {
+
+	namespace.init = function(app) {
+		var node = app.get_node();
+		var page = app.get_page();
+	};
+
+});
