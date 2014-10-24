923501d45182ab3be733eec117ff147ae9ec22fd
nallagsoc <vivek.candid@gmail.com>
2014-10-24 07:33:56 -0500

    modified initial file

--- holidayListFinal.js
+++ holidayListFinal.js
@@ -15,9 +15,7 @@ select_namespace(
 				return {
 					"vendor" : {
 						"271" : {
-														"weekends" : [ "false", "true", "true", "true",
-									"true", "true", "false" ],
-							"eventDate" : [ "2 Jan 2012", "26 Jan 2012",
+																					"eventDate" : [ "2 Jan 2012", "26 Jan 2012",
 									"12 Mar 2012", "6 Apr 2012", "7 Apr 2012",
 									"9 Apr 2012", "25 Apr 2012", "11 Jun 2012",
 									 ]
